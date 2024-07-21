15ac307441f14bb3c607d890188f0e268def8665
Pratik Sahoo <iampratiksahoo@gmail.com>
2024-07-21 11:39:52 +0530

    polish 1

---
+++ Assets/Scripts/billboard_anim.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using UnityEngine;
+
+public class billboard_anim : MonoBehaviour
+{
+    public AudioSource source;
+    public GameObject on;
+    public AudioClip on_clip;
+    public AudioClip off_clip;
+
+    public float delay = 1f;
+
+    private void Start()
+    {
+        StartCoroutine(anim());
+    }
+
+    IEnumerator anim()
+    {
+        while (true)
+        {
+            if(on.activeInHierarchy)
+            {
+                source.clip = off_clip;
+                on.SetActive(false);
+            }
+            else
+            {
+                source.clip = on_clip;
+                on.SetActive(true);
+            }
+            source.Play();
+            yield return new WaitForSeconds(delay);
+        }
+    }
+}
